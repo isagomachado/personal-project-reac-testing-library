3fb7e537a8348f6cde413642b7766b261883ee8f
isagomachado <isa_gomes_machado@hotmail.com>
2022-03-23 16:05:28 -0300

    Requisito 4 - feito

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+test('A página contém um h2 com o texto Page requested not found 😭', () => {
+  renderWithRouter(<NotFound />);
+
+  const headingtext = screen.getByRole('heading', { name: /Page requested not found/i });
+  const images = screen.getAllByRole('img');
+
+  expect(headingtext).toBeInTheDocument();
+  expect(images[0]).toBeInTheDocument();
+});
+
+test('A página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+  renderWithRouter(<NotFound />);
+
+  const images = screen.getAllByRole('img');
+
+  expect(images[1].src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
